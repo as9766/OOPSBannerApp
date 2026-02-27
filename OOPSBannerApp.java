5b1ba401be7966f733aaa84cdc68194ec45f1adb
as9766 <as9766@srmist.edu.in>
2026-02-27 23:37:00 +0530

    ADDED

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,41 +1,82 @@
-public class OOPSBannerApp{
-    public static String[] getOPattern() {
-        return new String[] {
-            "   ***   ",
-            " **   ** ",
-            "**     **",
-            "**     **",
-            "**     **",
-            " **   ** ",
-            "   ***   "
-        };  
-    }
-        public static String[] getPPattern() {
-        return new String[] {
-            "********",
-            "**    **",
-            "**    **",
-            "********",
-            "**      ",
-            "**      "
-        };
-    }
-        public static String[] getSPattern() {
-        return new String[] {
-            "********",
-            "*       ",
-            "*       ",
-            "********",
-            "       *",
-            "********"
-        };
-        public static void main(String[] args){
-            String[] oPattern =getOPattern();
-            String[] pPattern =getPPattern();
-            String[] sPattern =getSPattern();
-            for(int i= 0;i< oPattern.length; i++){
-                System.out.println(oPattern[i]+ " "+ oPattern[i]+ " "+ sPattern[i]);
-            }
-            
+import java.util.HashMap;
+import java.util.Map;
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String word = "OOPS";
+        CharacterPatternMap patternMap = new CharacterPatternMap();
+
+        StringBuilder[] bannerLines = new StringBuilder[7];
+
+        for (int i = 0; i < 7; i++) {
+            bannerLines[i] = new StringBuilder();
         }
-}}
+
+        for (char ch : word.toCharArray()) {
+
+            String[] pattern = patternMap.getPattern(ch);
+
+            for (int i = 0; i < 7; i++) {
+                bannerLines[i].append(pattern[i]).append("   ");
+            }
+        }
+
+        for (StringBuilder line : bannerLines) {
+            System.out.println(line.toString());
+        }
+    }
+
+    // Static Inner Class
+    static class CharacterPatternMap {
+
+        private final Map<Character, String[]> patternStore;
+
+        public CharacterPatternMap() {
+            patternStore = new HashMap<>();
+
+            patternStore.put('O', new String[]{
+                    " ***** ",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    " ***** "
+            });
+
+            patternStore.put('P', new String[]{
+                    "****** ",
+                    "*     *",
+                    "*     *",
+                    "****** ",
+                    "*      ",
+                    "*      ",
+                    "*      "
+            });
+
+            patternStore.put('S', new String[]{
+                    " ***** ",
+                    "*     *",
+                    "*      ",
+                    " ***** ",
+                    "      *",
+                    "*     *",
+                    " ***** "
+            });
+        }
+
+        public String[] getPattern(char ch) {
+            return patternStore.getOrDefault(ch, new String[]{
+                    "       ",
+                    "       ",
+                    "       ",
+                    "       ",
+                    "       ",
+                    "       ",
+                    "       "
+            });
+        }
+    }
+}
